c41fe4190d4f332b5a18d5346ef8a55819fa49a7
ShaiOren4761 <shaioren100@gmail.com>
2026-08-18 15:14:02 +0300

    Assignment 2 completed

--- Assignment2_207667916/Game.cs
+++ Assignment2_207667916/Game.cs
@@ -2,11 +2,12 @@ namespace Assignment2_207667916
 {
     public partial class Game : Form
     {
-        private GameButton[,] Game_Matrix = new GameButton[4, 4];
+        private Button[] Game_Matrix = new Button[15];
         private int count = 1;
         Random r = new Random();
-        private GameButton[] To_Switch = new GameButton[2];
-        private int[] Locations = new int[4]; // b1 --> (x1,y1) - b2 --> (x2,y2)
+        private Button To_Switch = null;
+        private Point empty_pos = new Point();
+        private Point empty_next_pos = new Point();
         System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
 
         public Game()
@@ -36,9 +37,9 @@ namespace Assignment2_207667916
             this.Controls.Add(menuStrip);
         }
 
-        private GameButton CreateGameButton(int x, int y)
+        private Button CreateGameButton()
         {
-            GameButton b = new GameButton(x, y);
+            Button b = new Button();
             b.Height = 70;
             b.Width = 70;
             b.Text = count.ToString();
@@ -49,42 +50,32 @@ namespace Assignment2_207667916
             return b;
         }
 
-        private GameButton CreateGameButton_Invisible(int x, int y)
-        {
-            GameButton b = new GameButton(x, y);
-            b.Height = 70;
-            b.Width = 70;
-            b.Visible = false;
-            b.Name = "Empty";
-            b.Click += GameClick_Handler; // This control may become visible after a tile moves into this position.
-            return b;
-        }
-
         private void CreateBoard()
         {
             int x = 10, y = 50;
+            
             for (int i = 0; i < Game_Matrix.GetLength(0); i++)
             {
+                Game_Matrix[i] = CreateGameButton();
+                
+                Game_Matrix[i].Location = new Point(x + 20, y);
+                this.Controls.Add(Game_Matrix[i]);
 
-                for (int j = 0; j < Game_Matrix.GetLength(1); j++)
+                x += Game_Matrix[i].Width;
+
+                if ((i+1) % 4 == 0)
                 {
-                    if (i == 3 && j == 3) Game_Matrix[i, j] = CreateGameButton_Invisible(i, j);
-                    else Game_Matrix[i, j] = CreateGameButton(i, j);
-
-                    Game_Matrix[i, j].Location = new Point(x + 20, y);
-                    this.Controls.Add(Game_Matrix[i, j]);
-
-                    x += Game_Matrix[i, j].Width;
+                    x = 10;
+                    y += Game_Matrix[i].Height;
                 }
-                x = 10;
-                y += Game_Matrix[i, 0].Height;
             }
+            empty_pos = new Point(x + 20, y);
         }
 
         private void DestroyBoard()
         {
             count = 1;
-            foreach (GameButton g in Game_Matrix)
+            foreach (Button g in Game_Matrix)
             {
                 g.Dispose();
             }
@@ -92,49 +83,29 @@ namespace Assignment2_207667916
 
         private void RandomizePositions()
         {
-            int x, y;
+            int swp;
             for (int i = 0; i < Game_Matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < Game_Matrix.GetLength(1) && !(i == 3 && j == 3); j++)
+                do
                 {
-                    do
-                    {
-                        x = r.Next(4);
-                        y = r.Next(4);
-                    } while (x == 3 && y == 3); //Ignore bottom right
-                    Quick_Swap(Game_Matrix[i, j], Game_Matrix[x, y]);
-                }
+                    swp = r.Next(15);
+                } while (swp == i);
+                Quick_Swap(Game_Matrix[i], Game_Matrix[swp]);   
             }
         }
 
         private void GameClick_Handler(object sender, EventArgs e)
         {
-            GameButton b1 = sender as GameButton;
+            Button b = sender as Button;
 
-            if (b1 == null) return;
+            if (b == null) return;
 
-            int x, y;
-            if (IsAdjacentToEmpty(b1, out x, out y))
+            if (IsAdjacentToEmpty(b))
             {
                 // Trigger Switch Animation check
-                if (Animation_Swap(Game_Matrix[b1.X, b1.Y], Game_Matrix[x, y]))
-                {
-                    // Switch x,y in Matrix
-                    GameButton b2 = Game_Matrix[x, y];
-                    Game_Matrix[b1.X, b1.Y] = b2;
-                    Game_Matrix[x, y] = b1;
-
-                    // Switch x,y inside GameButtons
-                    int xTemp = b1.X;
-                    int yTemp = b1.Y;
-                    b1.X = b2.X;
-                    b1.Y = b2.Y;
-                    b2.X = xTemp;
-                    b2.Y = yTemp;
-                }
-
+                Animation_Swap(b);
             }
-            
+
         }
 
         private void NewGameClick_Handler(object sender, EventArgs e)
@@ -142,46 +113,22 @@ namespace Assignment2_207667916
             RestartGame();
         }
 
-        private void Quick_Swap(GameButton b1, GameButton b2)
+        private void Quick_Swap(Button b1, Button b2)
         {
-            bool b1Visible = !(b2.Name == "Empty");
-            bool b2Visible = !(b1.Name == "Empty");
+            // Animationless swap for game init
 
-            // Copy b1's properties into temp
-            String tempText = b1.Text;
-            Font tempFont = b1.Font;
-            Color tempBackColor = b1.BackColor;
-            String tempName = b1.Name;
-
-            // Copy b2's properties into b1
-            b1.Text = b2.Text;
-            b1.Font = b2.Font;
-            b1.BackColor = b2.BackColor;
-            b1.Name = b2.Name;
-
-            // Copy b1's properties into b2 (via temp)
-            b2.Text = tempText;
-            b2.Font = tempFont;
-            b2.BackColor = tempBackColor;
-            b2.Name = tempName;
-
-            // Set visibility
-            b1.Visible = b1Visible;
-            b2.Visible = b2Visible;
-            // Explicit check and not just copy, because of Form default visibility set to "false" until form itself is visible, which makes all button invisible at the start.
+            Point tmp = new Point(b1.Location.X, b1.Location.Y);
+            b1.Location = b2.Location;
+            b2.Location = tmp;
         }
 
-        private Boolean Animation_Swap(GameButton b1, GameButton b2)
+        private Boolean Animation_Swap(Button b)
         {
-            if (this.To_Switch[0] == null){ // Only add button to switch queue if empty.
-                // Btns to Switch
-                To_Switch[0] = b1;
-                To_Switch[1] = b2;
-                // Destinations
-                Locations[0] = b2.Left;
-                Locations[1] = b2.Top;
-                Locations[2] = b1.Left;
-                Locations[3] = b1.Top;
+            if (this.To_Switch == null)
+            { // Only add button to switch queue if empty.
+                // Btn to Switch
+                To_Switch = b;
+                empty_next_pos = b.Location; // Back b's position to transfer to empty when done swapping
                 // Start timer
                 t.Start();
                 return true;
@@ -189,59 +136,36 @@ namespace Assignment2_207667916
             return false;
         }
 
-        private Boolean IsAdjacentToEmpty(GameButton b, out int x_empty, out int y_empty)
+        private Boolean IsAdjacentToEmpty(Button b)
         {
-            int x = b.X, y = b.Y;
-            if (IsEmpty(x, y + 1))
-            {
-                x_empty = x;
-                y_empty = y + 1;
-                return true;
-            }
-            else if (IsEmpty(x, y - 1))
-            {
-                x_empty = x;
-                y_empty = y - 1;
-                return true;
-            }
-            else if (IsEmpty(x - 1, y))
-            {
-                x_empty = x - 1;
-                y_empty = y;
-                return true;
-            }
-            else if (IsEmpty(x + 1, y))
-            {
-                x_empty = x + 1;
-                y_empty = y;
-                return true;
-            }
-            x_empty = -1; y_empty = -1;
-            return false;
-        }
+            int x = b.Left; int y = b.Top;
 
-        private Boolean IsEmpty(int x, int y)
-        {
-            if (x >= 0 && y >= 0 && x <= Game_Matrix.GetUpperBound(0) && y <= Game_Matrix.GetUpperBound(1)) // check x,y bounds
+            if (empty_pos == new Point(x, y+b.Height) || empty_pos == new Point(x, y - b.Height) 
+                || empty_pos == new Point(x + b.Width, y) || empty_pos == new Point(x - b.Width, y))
             {
-                if (Game_Matrix[x, y].Name == "Empty") return true; // check button name
+                return true;
             }
             return false;
         }
 
         private void CheckWinCondition()
         {
-            // Basic required check by assignment
-            //if (Game_Matrix[0, 0].Text == "1" && Game_Matrix[0, 1].Text == "2") Win();
 
-            // Full Win Condition
-            int counter = 1;
-            for (int i = 0; i < Game_Matrix.GetLength(0); i++)
+            // i limit determines how many buttons need to be order to win.
+            Point ExpectedLocation = new Point();
+
+            for (int i = 1; i <= 2; i++) // <--- i <= amount of blocks to solve
             {
-                for (int j = 0; j < Game_Matrix.GetLength(1); j++)
+                for (int j = 0; j < Game_Matrix.Length; j++)
                 {
-                    if (counter == 16) continue;
-                    if (Game_Matrix[i, j].Text != "" + counter++) return;
+                    if (Game_Matrix[j].Text == i.ToString())
+                    {
+                        ExpectedLocation.X = 30 + (Game_Matrix[j].Width * ((i - 1) % 4));
+                        ExpectedLocation.Y = 50 + (Game_Matrix[j].Height * ((int)(i - 1) / 4));
+
+                        if (ExpectedLocation != Game_Matrix[j].Location) return;
+                        break;
+                    }
                 }
             }
             Win();
@@ -258,9 +182,8 @@ namespace Assignment2_207667916
         private void RestartGame()
         {
             t.Stop(); // Stop current board actions before deleting it.
-            To_Switch[0] = null;
-            To_Switch[1] = null;
-
+            To_Switch = null;
+            
             DestroyBoard();
             CreateBoard();
             RandomizePositions();
@@ -274,68 +197,46 @@ namespace Assignment2_207667916
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (To_Switch[0] != null)
+            if (To_Switch != null)
             {
-                if (To_Switch[0].Left != Locations[0] || To_Switch[0].Top != Locations[1]) // Buttons not in correct position
+                if (To_Switch.Location != empty_pos) // Button at destination?
                 {
                     AdjustPositions();
                 }
-                else // Btns have switched positions
+                else // Btn is at destination
                 {
                     t.Stop();
+                    empty_pos = empty_next_pos; // complete swap
                     CheckWinCondition();
-                    To_Switch[0] = null;
-                    To_Switch[1] = null;
+                    To_Switch = null;
                 }
             }
         }
 
         private void AdjustPositions()
         {
-            // Locations [0],[1] = Destination for b1
-            // Locations [2],[3] = Destination for b2
-
-            GameButton b1 = To_Switch[0];
-            GameButton b2 = To_Switch[1];
-
-            if (b1.Left < Locations[0])
+            Button b = To_Switch;
+            
+            if (b.Left < empty_pos.X)
             {
-                b1.Left += 10;
+                b.Left += 10;
             }
-            else if (b1.Left > Locations[0])
+            else if (b.Left > empty_pos.X)
             {
-                b1.Left -= 10;
+                b.Left -= 10;
             }
-
-            if (b1.Top < Locations[1])
+            else if (b.Top < empty_pos.Y)
             {
-                b1.Top += 10;
+                b.Top += 10;
             }
-            else if (b1.Top > Locations[1])
+            else if (b.Top > empty_pos.Y)
             {
-                b1.Top -= 10;
+                b.Top -= 10;
             }
 
-            if (b2.Left < Locations[2])
-            {
-                b2.Left += 10;
-            }
-            else if (b2.Left > Locations[2])
-            {
-                b2.Left -= 10;
-            }
-
-            if (b2.Top < Locations[3])
-            {
-                b2.Top += 10;
-            }
-            else if (b2.Top > Locations[3])
-            {
-                b2.Top -= 10;
-            }
         }
     }
-        
 
-    
+
+
 }
